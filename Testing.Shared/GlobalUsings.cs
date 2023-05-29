7c778c0fe80579e0e235ced7cda47cacbc05e1b3
Arve Hansen <arve.hansen@unit4.com>
2023-05-29 10:35:06 -0400

    Refactored System.IO.Abstractions.TestingHelpers

--- Testing.Shared/GlobalUsings.cs
+++ Testing.Shared/GlobalUsings.cs
@@ -1,9 +1,10 @@
 // Global using directives
 
+global using ABulkCopy.ASqlServer.Column.ColumnTypes;
 global using ABulkCopy.Common.Config;
 global using ABulkCopy.Common.Types.Column;
 global using ABulkCopy.Common.Types.Table;
-global using ABulkCopy.ASqlServer.Column.ColumnTypes;
 global using Microsoft.Extensions.Configuration;
 global using System.Data.SqlClient;
+global using System.IO.Abstractions.TestingHelpers;
 global using System.Text;
