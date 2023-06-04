d48bb7b470d287f0ab12086efdf5f3f5247a29ed
Arve Hansen <arve.hansen@unit4.com>
2023-06-04 16:05:17 -0400

    Refactored PgSchemaReaderTests Tests for all SqlServer number types

--- APostgres.Test/PgSchemaReaderTests.cs
+++ APostgres.Test/PgSchemaReaderTests.cs
@@ -1,4 +1,6 @@
-﻿namespace APostgres.Test;
+﻿using ABulkCopy.ASqlServer.Column.ColumnTypes;
+
+namespace APostgres.Test;
 
 public class PgSchemaReaderTests : PgTestBase
 {
@@ -31,19 +33,66 @@ public class PgSchemaReaderTests : PgTestBase
     }
 
     [Fact]
-    public async Task TestBigInt()
+    public async Task ReadMssBigInt()
     {
-        var col = new PostgresBigInt(1, "MyTestCol", false);
-        var result = await GetColFromTableDefinition(col);
-        VerifyColumn(result, col);
+        var result = await GetColFromTableDefinition(new SqlServerBigInt(1, "MyTestCol", false));
+        VerifyColumn(result, new PostgresBigInt(1, "MyTestCol", false));
     }
 
     [Fact]
     public async Task TestInt()
     {
-        var col = new PostgresInt(1, "MyTestCol", false);
-        var result = await GetColFromTableDefinition(col);
-        VerifyColumn(result, col);
+        var result = await GetColFromTableDefinition(new SqlServerInt(1, "MyTestCol", false));
+        VerifyColumn(result, new PostgresInt(1, "MyTestCol", false));
+    }
+
+    [Fact]
+    public async Task TestSmallInt()
+    {
+        var result = await GetColFromTableDefinition(new SqlServerSmallInt(1, "MyTestCol", false));
+        VerifyColumn(result, new PostgresSmallInt(1, "MyTestCol", false));
+    }
+
+    [Fact]
+    public async Task TestTinyInt()
+    {
+        var result = await GetColFromTableDefinition(new SqlServerTinyInt(1, "MyTestCol", false));
+        VerifyColumn(result, new PostgresSmallInt(1, "MyTestCol", false));
+    }
+
+    [Fact]
+    public async Task TestBit()
+    {
+        var result = await GetColFromTableDefinition(new SqlServerBit(1, "MyTestCol", false));
+        VerifyColumn(result, new PostgresBoolean(1, "MyTestCol", false));
+    }
+
+    [Fact]
+    public async Task TestDecimal()
+    {
+        var result = await GetColFromTableDefinition(new SqlServerDecimal(1, "MyTestCol", false, 32, 4));
+        VerifyColumn(result, new PostgresDecimal(1, "MyTestCol", false, 32, 4));
+    }
+
+    [Fact]
+    public async Task TestMoney()
+    {
+        var result = await GetColFromTableDefinition(new SqlServerMoney(1, "MyTestCol", false));
+        VerifyColumn(result, new PostgresMoney(1, "MyTestCol", false));
+    }
+
+    [Fact]
+    public async Task TestReal()
+    {
+        var result = await GetColFromTableDefinition(new SqlServerFloat(1, "MyTestCol", false, 13));
+        VerifyColumn(result, new PostgresReal(1, "MyTestCol", false));
+    }
+
+    [Fact]
+    public async Task TestFloat()
+    {
+        var result = await GetColFromTableDefinition(new SqlServerFloat(1, "MyTestCol", false, 32));
+        VerifyColumn(result, new PostgresDoublePrecision(1, "MyTestCol", false));
     }
 
     private static void VerifyColumn(IColumn actual, IColumn expected)
