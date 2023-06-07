60d7548f5ee26e38b3f20ab52e4fbd69ec62b8bb
Arve Hansen <arve.hansen@unit4.com>
2023-06-07 11:45:28 -0400

    TestDate

---
+++ APostgres.Test/DataReader/PgDataReaderDateTimeTests.cs
@@ -0,0 +1,25 @@
+﻿namespace APostgres.Test.DataReader;
+
+public class PgDataReaderDateTimeTests : PgDataReaderTestBase
+{
+    private const string ColName = "Col1";
+
+    public PgDataReaderDateTimeTests(ITestOutputHelper output)
+        : base(output, nameof(PgDataReaderDateTimeTests))
+    {
+    }
+
+    [Fact]
+    public async Task TestDate()
+    {
+        // Arrange
+        var testVal = new DateOnly(2023, 06, 14);
+        var col = new PostgresDate(1, ColName, false);
+        var colValue = await TestDataReader<DateOnly>(
+            col, "2023-06-14,");
+
+        colValue.Should().Be(testVal);
+    }
+
+
+}
