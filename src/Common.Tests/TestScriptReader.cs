c3807f16c27f40803aac5b98890b60808ffa8241
Arve Hansen <arveh4@outlook.com>
2025-05-25 17:28:20 +0200

    Refactored TestScriptReader to use Theory

--- src/Common.Tests/TestScriptReader.cs
+++ src/Common.Tests/TestScriptReader.cs
@@ -19,48 +19,19 @@ public class TestScriptReader
         await act.Should().ThrowAsync<FileNotFoundException>()
             .WithMessage("File TestScriptReader.sql not found");
     }
-    
-    [Fact]
-    public async Task TestRead_When_ScriptFileEmpty()
+
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("Statement1", 1)]
+    [InlineData("Statement1\n\nStatement2\n", 2)]
+    [InlineData("Statement1\nMoreStatement12", 1)]
+    public async Task TestRead(string fileContent, int expectedNoOfStatements)
     {
-        var fileData = new MockFileData(string.Empty, Encoding.UTF8);
+        var fileData = new MockFileData(fileContent, Encoding.UTF8);
         _fileHelper.FileSystem.AddFile(TestFileName, fileData);
         
         var sqlStatements = await _scriptsReader.ReadAsync(TestFileName).ToListAsync();
 
-        sqlStatements.Should().BeEmpty();
-    }
-
-    [Fact]
-    public async Task TestReadSingleStatement_When_MissingEndOfStatement_Then_Ok()
-    {
-        var fileData = new MockFileData("first statement", Encoding.UTF8);
-        _fileHelper.FileSystem.AddFile(TestFileName, fileData);
-         
-        var sqlStatements = await _scriptsReader.ReadAsync(TestFileName).ToListAsync();
-
-        sqlStatements.Count.Should().Be(1);
-    }
-
-    [Fact]
-    public async Task TestReadSeveralStatements_When_MissingEndOfLastStatement_Then_Ok()
-    {
-        var fileData = new MockFileData("statement1;" + Environment.NewLine + Environment.NewLine + "statement2;" + Environment.NewLine, Encoding.UTF8);
-        _fileHelper.FileSystem.AddFile(TestFileName, fileData);
-        
-        var sqlStatements = await _scriptsReader.ReadAsync(TestFileName).ToListAsync();
-
-        sqlStatements.Count.Should().Be(2);
-    }
-
-    [Fact]
-    public async Task TestSingleNewLine_Then_SingleStatement()
-    {
-        var fileData = new MockFileData("statement1;" + Environment.NewLine + "more_statement1;", Encoding.UTF8);
-        _fileHelper.FileSystem.AddFile(TestFileName, fileData);
-        
-        var sqlStatements = await _scriptsReader.ReadAsync(TestFileName).ToListAsync();
-        
-        sqlStatements.Count.Should().Be(1);
+        sqlStatements.Count.Should().Be(expectedNoOfStatements);
     }
 }
