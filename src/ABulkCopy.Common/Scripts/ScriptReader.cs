4714715d921e6187e0e7201d1055148614090ee3
Arve Hansen <arveh4@outlook.com>
2025-05-25 15:40:18 +0200

    ScriptReader ReadStatement

--- src/ABulkCopy.Common/Scripts/ScriptReader.cs
+++ src/ABulkCopy.Common/Scripts/ScriptReader.cs
@@ -15,8 +15,43 @@ public class ScriptReader
         {
             throw new FileNotFoundException($"File {scriptFile} not found");
         }
-       
-        await Task.CompletedTask;
-        yield break;
+
+        var sb = new StringBuilder();
+
+        var file = _fileSystem.File.OpenText(scriptFile);
+        while (!file.EndOfStream)
+        {
+            var stmt = await ReadStatementAsync(file, sb);
+            if (string.IsNullOrEmpty(stmt))
+            {
+                yield break;
+            }
+
+            yield return stmt;
+        }
+    }
+
+    private static async Task<string?> ReadStatementAsync(
+        StreamReader file, 
+        StringBuilder sb)
+    {
+        sb.Clear();
+        if (file.EndOfStream)
+        {
+            return sb.ToString();
+        }
+        
+        var line = await file.ReadLineAsync();
+        while (!string.IsNullOrEmpty(line))
+        {
+            sb.AppendLine(line); 
+            if (file.EndOfStream)
+            {
+                return sb.ToString();
+            }
+            line = await file.ReadLineAsync();
+        }
+
+        return sb.ToString();
     }
 }
--- src/Common.Tests/TestScriptReader.cs
+++ src/Common.Tests/TestScriptReader.cs
@@ -45,7 +45,7 @@ public class TestScriptReader
     }
 
     [Fact]
-    public async Task TestReadSeveralLines_When_MissingEndOfStatement_Then_SingleStatement()
+    public async Task TestReadSeveralLines_When_MissingEndOfStatement_Then_Ok()
     {
         var fileHelper = new FileHelper();
         var fileData = new MockFileData("statement1;" + Environment.NewLine + Environment.NewLine + "statement2;" + Environment.NewLine, Encoding.UTF8);
@@ -54,6 +54,19 @@ public class TestScriptReader
         
         var sqlStatements = await scriptsReader.ReadAsync(TestFileName).ToListAsync();
 
+        sqlStatements.Count.Should().Be(2);
+    }
+
+    [Fact]
+    public async Task TestSingleNewLine_Then_SingleStatement()
+    {
+        var fileHelper = new FileHelper();
+        var fileData = new MockFileData("statement1;" + Environment.NewLine + "statement2;", Encoding.UTF8);
+        fileHelper.FileSystem.AddFile("TestScriptReader.sql", fileData);
+        var scriptsReader = new ScriptReader(fileHelper.FileSystem);
+        
+        var sqlStatements = await scriptsReader.ReadAsync(TestFileName).ToListAsync();
+        
         sqlStatements.Count.Should().Be(1);
     }
 }
