5430479e9426b32b99ff73c75e10e1695adaa675
Arve Hansen <arveh4@outlook.com>
2025-08-17 11:05:42 +0200

    TestGetPrimaryKey_When_Exists

---
+++ src/Postgres.Tests/SystemTables/PgTestPrimaryKey.cs
@@ -0,0 +1,47 @@
+namespace Postgres.Tests.SystemTables;
+
+[Collection(nameof(DatabaseCollection))]
+public class PgTestPrimaryKey : PgTestBase
+{
+    private readonly IIdentifier _identifier;
+    public PgTestPrimaryKey(DatabaseFixture dbFixture, ITestOutputHelper output) 
+        : base(dbFixture, output)
+    {
+        _identifier = GetIdentifier();
+    }
+    
+    [Fact]
+    public async Task TestGetPrimaryKey_When_Exists()
+    {
+        // Arrange
+        var tableName = GetName();
+        await DropTableAsync(DatabaseFixture.DefaultSchemaName, tableName);
+        await ExecuteNonQueryAsync(
+            $"CREATE TABLE {tableName}(Key1 int NOT NULL, Key2 int NOT NULL, AnotherCol varchar(20), " + Environment.NewLine +
+            "PRIMARY KEY (Key1, Key2))");
+
+        var tableHeader = await PgSystemTables.GetTableHeaderAsync(
+            DatabaseFixture.DefaultSchemaName, tableName, CancellationToken.None);
+        tableHeader.Should().NotBeNull();
+
+        // Act
+        var pk = await PgSystemTables.GetPrimaryKeyAsync(tableHeader!, CancellationToken.None);
+
+        // Assert
+        pk.Should().NotBeNull();
+        pk!.ColumnNames.Count.Should().Be(2);
+        pk.ColumnNames[0].Name.Should().Be(_identifier.AdjustForSystemTable("Key1"));
+        pk.ColumnNames[0].Direction.Should().Be(Direction.Ascending);
+        pk.ColumnNames[1].Name.Should().Be(_identifier.AdjustForSystemTable("Key2"));
+        pk.ColumnNames[1].Direction.Should().Be(Direction.Ascending);
+    }
+    
+    private static IIdentifier GetIdentifier()
+    {
+        Dictionary<string, string?> appSettings = new()
+        {
+            { Constants.Config.AddQuotes, "false" },
+        };
+        return GetIdentifier(appSettings);
+    }
+}
